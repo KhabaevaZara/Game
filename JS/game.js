37c0f7ba1aff8333ecad0e0522ec4b5bbf53d985
ZaraHabaeva <zarahabaeva12@yahoo.com>
2025-06-26 10:35:44 +0300

    1

--- JS/game.js
+++ JS/game.js
@@ -7,7 +7,9 @@ let createRect = (x ,y , width, height, color) => {
     canvasContext.fillStyle = color;
     canvasContext.fillRect(x,y , width , height);
 };
-
+let fps =  30;
+let oneBlockSize = 20;
+let wallColor =  "#342DCA";
 let map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
@@ -37,8 +39,32 @@ let map = [
      update ()
      draw ()
 
- }
+ };
 
  let update = () => {
     //todo
- }
+ };
+
+ let draw = () =>{
+    createRect(0,0 , canvas.width , canvas.height, "black"  );
+    //todo
+    drawWalls()
+ };
+
+ let gameInterval = setInteval(gameLoop, 1000/fps);
+
+ let drawWalls = () => {
+  for (let i = 0 ; i < map.length ; i++) {
+    for (let j = 0; j < map[0].length; j++){
+        if (map[i][j] == 1){ //then it is a wall
+            createRect( 
+                j * oneBlockSize ,
+                 i * oneBlockSize ,
+                  oneBlockSize, 
+                  oneBlockSize,
+                  wallColor
+                );
+        }
+    }
+  }
+ };
